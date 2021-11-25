0ce940d738a8091c9383cfc99a78c29388137244
Gabbe1991 <gabe.jara@gmail.com>
2021-11-25 14:35:59 +0100

    Done

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+const divTest = document.getElementById("test");
+const btnHigher = document.getElementById("higher");
+const btnLower = document.getElementById("lower");
+const card = document.getElementById("card");
+const nextcard = document.getElementById("nextcard");
+const result = document.getElementById("result");
+const img = document.getElementById("img");
+
+btnHigher.addEventListener("click", higher);
+btnLower.addEventListener("click", lower);
+5
+
+start();
+
+async function start(){
+	await createDeck();
+	await getCard(divTest.getAttribute("deckid"));
+}
+
+3
+async function higher(){
+	await getNextCard(divTest.getAttribute("deckid"));
+	
+	if(parseInt(card.getAttribute("cardVal")) >= parseInt(nextcard.getAttribute("cardVal"))){
+		result.innerText = "Lose";
+	} else {
+		result.innerText = "Win";
+	}
+}
+3
+async function lower(){
+	await getNextCard(divTest.getAttribute("deckid"));
+	
+	if(parseInt(card.getAttribute("cardVal")) <= parseInt(nextcard.getAttribute("cardVal"))){
+		result.innerText = "Lose";
+	} else {
+		result.innerText = "Win";
+	}
+}
+
+1
 async function createDeck() {
 	const myObj = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
 	const myText = await myObj.json();
@@ -6,7 +47,8 @@ async function createDeck() {
 	divTest.setAttribute("DeckID", myText.deck_id)
 }
 
-async function getCard(deckID) {
+2
+ async function getCard(deckID) {
 	const myObj = await fetch("https://deckofcardsapi.com/api/deck/" + deckID + "/draw/?count=1");
 	const myText = await myObj.json();
 	
@@ -27,6 +69,7 @@ async function getCard(deckID) {
 	img.setAttribute("src", myText.cards[0].image);
 	nextcard.innerText = "Current Card: " + tmp;
 }
+3
 async function getNextCard(deckID) {
 	card.setAttribute("cardVal", nextcard.getAttribute("cardVal"));
 	
@@ -50,3 +93,5 @@ async function getNextCard(deckID) {
 	img.setAttribute("src", myText.cards[0].image);
 	nextcard.setAttribute("cardVal", tmp);
 }
+
+
