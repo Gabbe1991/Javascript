4f9fb0e1f484c74014f8026edeb0669ef18d9e4e
Gabbe1991 <gabe.jara@gmail.com>
2021-11-25 13:53:35 +0100

    getnextCard

--- index.js
+++ index.js
@@ -27,3 +27,26 @@ async function getCard(deckID) {
 	img.setAttribute("src", myText.cards[0].image);
 	nextcard.innerText = "Current Card: " + tmp;
 }
+async function getNextCard(deckID) {
+	card.setAttribute("cardVal", nextcard.getAttribute("cardVal"));
+	
+	const myObj = await fetch("https://deckofcardsapi.com/api/deck/" + deckID + "/draw/?count=1");
+	const myText = await myObj.json();
+	
+	
+	var tmp = myText.cards[0].value;
+	
+	if(tmp == "KING"){
+		tmp = 13;
+	} else if (tmp == "QUEEN"){
+		tmp = 12;
+	} else if (tmp == "JACK"){
+		tmp = 11;
+	} else if (tmp == "ACE"){
+		tmp = 1;
+	}
+	4
+	nextcard.innerText = "Current Card: " + tmp;
+	img.setAttribute("src", myText.cards[0].image);
+	nextcard.setAttribute("cardVal", tmp);
+}
