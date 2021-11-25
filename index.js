ea9f37979026c9d8d7f675d53ce83e1e2d3bb76f
Gabbe1991 <gabe.jara@gmail.com>
2021-11-25 13:22:38 +0100

    if else

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ async function createDeck() {
 	divTest.innerText = "Deck ID: " + myText.deck_id;
 	divTest.setAttribute("DeckID", myText.deck_id)
 }
+
+async function getCard(deckID) {
+	const myObj = await fetch("https://deckofcardsapi.com/api/deck/" + deckID + "/draw/?count=1");
+	const myText = await myObj.json();
+	
+	
+	var tmp = myText.cards[0].value;
+	
+	if(tmp == "KING"){
+		tmp = 13;
+	} else if (tmp == "QUEEN"){
+		tmp = 12;
+	} else if (tmp == "JACK"){
+		tmp = 11;
+	} else if (tmp == "ACE"){
+		tmp = 1;
+	}
+	
+	card.setAttribute("cardVal", tmp);
+	img.setAttribute("src", myText.cards[0].image);
+	nextcard.innerText = "Current Card: " + tmp;
+}
