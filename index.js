3919a48dc39d3f777a62fc7765ace529e0852001
Gabbe1991 <gabe.jara@gmail.com>
2021-11-25 12:01:23 +0100

    kort

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("I work")
+async function createDeck() {
+	const myObj = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+	const myText = await myObj.json();
+
+	divTest.innerText = "Deck ID: " + myText.deck_id;
+	divTest.setAttribute("DeckID", myText.deck_id)
+}
